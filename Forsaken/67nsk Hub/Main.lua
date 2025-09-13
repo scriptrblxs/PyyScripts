1f6853fcd3f718bd550224dae23c503ebfbd70c6
scriptrblxs <calapan071116@gmail.com>
2025-09-13 19:33:05 +0800

    script overhaul + john doe, 1x, slasher

--- Forsaken/67nsk Hub/Main.lua
+++ Forsaken/67nsk Hub/Main.lua
@@ -1,118 +1,133 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local SelectedRunAnimation = "Survivor"
-local SelectedWalkAnimation = "Survivor"
-local SelectedIdleAnimation = "Survivor"
+local SelectedAnimations = {
+    Run = "Survivor",
+    Walk = "Survivor",
+    Idle = "Survivor"
+}
+
+local AnimationOptions = {"Survivor", "Noli", "John Doe", "Slasher", "1x1x1x1"}
+
+local AnimationIds = {
+    Run = {
+        Survivor = "rbxassetid://136252471123500",
+        Noli = "rbxassetid://117451341682452",
+        ["John Doe"] = "rbxassetid://132653655520682",
+        Slasher = "rbxassetid://93054787145505",
+        ["1x1x1x1"] = "rbxassetid://106485518413331",
+    },
+    Walk = {
+        Survivor = "rbxassetid://108018357044094",
+        Noli = "rbxassetid://109700476007435",
+        ["John Doe"] = "rbxassetid://81193817424328",
+        Slasher = "rbxassetid://93622022596108",
+        ["1x1x1x1"] = "rbxassetid://109130982296927",
+    },
+    Idle = {
+        Survivor = "rbxassetid://131082534135875",
+        Noli = "rbxassetid://83465205704188",
+        ["John Doe"] = "rbxassetid://105880087711722",
+        Slasher = "rbxassetid://116050994905421",
+        ["1x1x1x1"] = "rbxassetid://138754221537146",
+    }
+}
 
 local Window = Rayfield:CreateWindow({
-   Name = "67nsk",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "67nsk le sigmabroy",
-   LoadingSubtitle = "by aerialasf",
-   ShowText = "67", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Amethyst", -- Check https://docs.sirius.menu/rayfield/configuration/themes
-
-   ToggleUIKeybind = Enum.KeyCode.F5, -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
-
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = "IMTHEsigmaestSAKENER", -- Create a custom folder for your hub/game
-      FileName = "ch7_0"
-   },
-
-   Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Hey you yes you i dont trust you",
-      Subtitle = "ligma key",
-      Note = "i dont fuckign know maybe join the discord server", -- Use this to tell the user how to get a key
-      FileName = "SIGMASIGMASakenkeyLIGMALIGMAohmy67", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Forsake thee"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+    Name = "67nsk",
+    Icon = 0,
+    LoadingTitle = "67nsk le sigmabroy",
+    LoadingSubtitle = "by aerialasf",
+    ShowText = "67",
+    Theme = "Amethyst",
+    ToggleUIKeybind = Enum.KeyCode.F5,
+    DisableRayfieldPrompts = false,
+    DisableBuildWarnings = false,
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = "IMTHEsigmaestSAKENER",
+        FileName = "ch7_0"
+    },
+    Discord = {
+        Enabled = false,
+        Invite = "noinvitelink",
+        RememberJoins = true
+    },
+    KeySystem = true,
+    KeySettings = {
+        Title = "Hey you yes you i dont trust you",
+        Subtitle = "ligma key",
+        Note = "i dont fuckign know maybe join the discord server",
+        FileName = "SIGMASIGMASakenkeyLIGMALIGMAohmy67",
+        SaveKey = true,
+        GrabKeyFromSite = false,
+        Key = {"Forsake thee"}
+    }
 })
 
-local Animations = Window:CreateTab("Animations", "a-large-small")
+local AnimationsTab = Window:CreateTab("Animations", "a-large-small")
 
-local testbtn = Animations:CreateButton({
+AnimationsTab:CreateButton({
     Name = "SIGMA BUTTON PLS CLICK",
     Callback = function()
         print("thanks")
     end
 })
 
-local RunDropdown = Animations:CreateDropdown({
-   Name = "Target Run Animation",
-   Options = {
-       "Survivor",
-       "This",
-       "Dropdown",
-       "Does",
-       "Nothing",
-       "Yet",
-   },
-   CurrentOption = {"Survivor"},
-   MultipleOptions = false,
-   Flag = "SurvivorRunAnimationId", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Options)
-       local optn = Options[1]
-       SelectedRunAnimation = optn
-   end,
-})
-
-local WalkDropdown = Animations:CreateDropdown({
-   Name = "Target Walk Animation",
-   Options = {
-       "Survivor",
-       "This",
-       "Dropdown",
-       "Does",
-       "Nothing",
-       "Yet",
-   },
-   CurrentOption = {"Survivor"},
-   MultipleOptions = false,
-   Flag = "SurvivorWalkAnimationId", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Options)
-       local optn = Options[1]
-       SelectedWalkAnimation = optn
-   end,
-})
-
-local IdleDropdown = Animations:CreateDropdown({
-   Name = "Target Idle Animation",
-   Options = {
-       "Survivor",
-       "This",
-       "Dropdown",
-       "Does",
-       "Nothing",
-       "Yet",
-   },
-   CurrentOption = {"Survivor"},
-   MultipleOptions = false,
-   Flag = "SurvivorIdleAnimationId", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Options)
-       local optn = Options[1]
-       SelectedIdleAnimation = optn
-   end,
-})
-
-local i = 0
-while true do
-    i += 1
-    print(tick() .. " " .. i)
-    print(SelectedRunAnimation)
-    print(SelectedWalkAnimation)
-    print(SelectedIdleAnimation)
-    task.wait(1)
+local function CreateAnimationDropdown(animType)
+    AnimationsTab:CreateDropdown({
+        Name = "Target " .. animType .. " Animation",
+        Options = AnimationOptions,
+        CurrentOption = {SelectedAnimations[animType]},
+        MultipleOptions = false,
+        Flag = animType .. "AnimationFlag",
+        Callback = function(Options)
+            SelectedAnimations[animType] = Options[1]
+        end
+    })
 end
+
+for _, animType in ipairs({"Run", "Walk", "Idle"}) do
+    CreateAnimationDropdown(animType)
+end
+
+local lplr = game.Players.LocalPlayer
+
+local function playAnimation(hum, id, weight, speed)
+    for _, track in pairs(hum:GetPlayingAnimationTracks()) do
+        if track.Animation.AnimationId == id then
+            track:AdjustSpeed(speed or 1)
+            return track
+        else
+            track:Stop(0.35)
+        end
+    end
+    local anim = Instance.new("Animation")
+    anim.AnimationId = id
+    local track = hum:LoadAnimation(anim)
+    track:AdjustWeight(weight or 10)
+    track:Play(0.35)
+    if speed then track:AdjustSpeed(speed) end
+    return track
+end
+
+local function setupCharacter(chr)
+    local hum = chr:WaitForChild("Humanoid")
+    hum.Running:Connect(function(spd)
+        local sprinting = chr:FindFirstChild("SpeedMultipliers") 
+            and chr.SpeedMultipliers:FindFirstChild("Sprinting") 
+            and chr.SpeedMultipliers.Sprinting.Value >= 1.3125
+
+        if spd > 0.01 then
+            if sprinting then
+                playAnimation(hum, AnimationIds.Run[SelectedAnimations.Run] or AnimationIds.Run.Survivor, 10, spd / 26)
+            else
+                playAnimation(hum, AnimationIds.Walk[SelectedAnimations.Walk] or AnimationIds.Walk.Survivor, 10, spd / 12)
+            end
+        else
+            playAnimation(hum, AnimationIds.Idle[SelectedAnimations.Idle] or AnimationIds.Idle.Survivor, 9, 1)
+        end
+    end)
+end
+
+if lplr.Character then setupCharacter(lplr.Character) end
+lplr.CharacterAdded:Connect(setupCharacter)
