441465163e5c8cd99812a368970ea32bc4a5d13a
callme_aerial <152961102+aerialasf@users.noreply.github.com>
2025-09-23 12:37:52 +0800

    Create Launcher.lua

---
+++ Forsaken/67nsk Hub/Launcher.lua
@@ -0,0 +1,7 @@
+local uis = game:GetService("UserInputService")
+
+if uis.TouchEnabled then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptrblxs/PyyScripts/refs/heads/main/Forsaken/67nsk%20Hub/v0.9.10-Mobile.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptrblxs/PyyScripts/refs/heads/main/Forsaken/67nsk%20Hub/v0.9.10-PC.lua"))()
+end
